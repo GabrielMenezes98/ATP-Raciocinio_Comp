f86a7042a75f8a16547e97c0cfe31840412d380a
Gabriel Menezes <gabrielmenezes6916@gmail.com>
2023-04-23 23:01:45 -0300

    Atualizado o mecanismo de salvar arquivos
    Implementado o menu inicial para turmas e refeita a estrutura de armazenamento do código.

--- main_GestAcademicData.py
+++ main_GestAcademicData.py
@@ -6,8 +6,8 @@
 import json
 
 ##########  CARREGA DADOS DO ARQUIVO EM ARMAZENAMENTO  ##########
-def carrega_dados():    
-    if op == 1:
+def carrega_dados_estudantes():   
+    ########## CARREGA DADOS DOS ESTUDANTES ######### 
         try:
             with open('dados_estudantes.json','r',encoding='utf8') as f:
                 dados_estudantes = json.load(f)
@@ -16,7 +16,9 @@ def carrega_dados():
             dados_estudantes = []
         return dados_estudantes
     
-    elif op == 2:
+    
+########## CARREGA DADOS DOS PROFESSORES ##########
+def carrega_dados_professores():  
         try:
             with open('dados_professores.json','r',encoding='utf8') as f:
                 dados_professores = json.load(f)
@@ -24,8 +26,10 @@ def carrega_dados():
         except FileNotFoundError:    
             dados_professores = []
         return dados_professores
-    
-    elif op == 3:
+
+
+########## CARREGA DADOS DAS DISCIPLINAS ##########
+def carrega_dados_disciplinas(): 
         try:
             with open('dados_disciplinas.json','r', encoding='utf8') as f:
                 dados_disciplinas = json.load(f)
@@ -33,8 +37,10 @@ def carrega_dados():
         except FileNotFoundError:    
             dados_disciplinas = []
         return dados_disciplinas
-    
-    elif op == 4:
+
+
+########## CARREGA DADOS DAS TURMAS ##########
+def carrega_dados_turmas():
         try:
             with open('dados_turmas.json','r', encoding='utf8') as f:
                 dados_turmas = json.load(f)
@@ -42,8 +48,10 @@ def carrega_dados():
         except FileNotFoundError:    
             dados_turmas = []
         return dados_turmas
-    
-    elif op == 5:
+
+
+########## CARREGA DADOS DAS MATRÍCULAS ##########
+def carrega_dados_matriculas():
         try:
             with open('dados_matriculas.json','r', encoding='utf8') as f:
                 dados_matriculas = json.load(f)
@@ -53,26 +61,26 @@ def carrega_dados():
         return dados_matriculas
 
 
+
 ##########  GRAVA DADOS NO ARQUIVO EM ARMAZENAMENTO  ##########
-def registra_dados(dados_estudantes, dados_professores, dados_disciplinas,dados_turmas,dados_matriculas):
-    if op == 1:
+def registra_dados_estudantes(dados_estudantes):
         with open('dados_estudantes.json','w',encoding='utf8') as f:
             json.dump(dados_estudantes,f, ensure_ascii=False)
     
-    if op == 2:
+def registra_dados_professores(dados_professores):
         with open('dados_professores.json','w',encoding='utf8') as f:
             json.dump(dados_professores,f, ensure_ascii=False)
 
-    if op == 3:
+def registra_dados_disciplinas(dados_disciplinas):
         with open('dados_disciplinas.json','w',encoding='utf8') as f:
             json.dump(dados_disciplinas,f, ensure_ascii=False)
 
-    if op == 4:
-        with open('dads_turmas.json','w',encoding='utf8') as f:
+def registra_dados_turmas(dados_turmas):
+        with open('dados_turmas.json','w',encoding='utf8') as f:
             json.dump(dados_turmas,f, ensure_ascii=False)
 
-    if op == 5:
-        with open('dads_matriculas.json','w',encoding='utf8') as f:
+def registra_dados_matriculas(dados_matriculas):
+        with open('dados_matriculas.json','w',encoding='utf8') as f:
             json.dump(dados_matriculas,f, ensure_ascii=False)
 
 
@@ -116,31 +124,22 @@ def inserir(op,dados_estudantes,dados_professores, dados_disciplinas, dados_turm
     elif op == 4:
         while True:
             codigo = int(input("Insira o código da turma: "))
-            if valida_cod_professores(codigo, dados_professores):
-                continue
-            nome = input("Insira o nome do professor: ")
-            cpf =input("Insira o CPF: ")
-            infoprofessor = {"Código":codigo,"Nome":nome,"CPF":cpf}
-            dados_professores.append(infoprofessor)
-            if input("Deseja inserir outro professor?(s/n) ") == "n":
+            insere_professores_em_turmas(dados_turmas, dados_professores)
+            if input("Deseja inserir outra turma?(s/n) ") == "n":
                 break
 
     elif op == 5:
         while True:
-            codigo = int(input("Insira o código do professor: "))
-            if valida_cod_professores(codigo, dados_professores):
-                continue
-            nome = input("Insira o nome do professor: ")
-            cpf =input("Insira o CPF: ")
-            infoprofessor = {"Código":codigo,"Nome":nome,"CPF":cpf}
-            dados_professores.append(infoprofessor)
-            if input("Deseja inserir outro professor?(s/n) ") == "n":
+            cod_codigo = int(input("Insira o código da turma: "))
+            #valid codigo turma
+            cod_estudante = int(input("Insira o código do estudante: "))
+            valida_cod_estudante()
+            if input("Deseja inserir outra matrícula(s/n) ") == "n":
                 break
         
 
 ##########  FUNÇÃO PARA LISTAGEM DE ESTUDANTES  ##########
 def listar_estudantes(dados_estudantes):
-    dados_estudantes = carrega_dados()
     if len(dados_estudantes) == 0:
         print("A lista está vazia")
     
@@ -151,7 +150,7 @@ def listar_estudantes(dados_estudantes):
 
 
 ##########  FUNÇÃO PARA ATUALIZAR ESTUDANTES  ##########
-def atualizar_estudante(dados_estudantes, dados_professores, dados_disciplinas, dados_turmas, dados_matriculas):
+def atualizar_estudante(dados_estudantes):
     codigo_att = int(input("Qual o código do estudante que deseja alterar?: "))
     novo_nome = input("Insira o nome: ")
     novo_cpf = input("Insira o CPF: ")
@@ -161,17 +160,17 @@ def atualizar_estudante(dados_estudantes, dados_professores, dados_disciplinas, 
         if estudante['Código'] == codigo_att:
             estudante['Nome'] = novo_nome
             estudante['CPF'] = novo_cpf
-            registra_dados(dados_estudantes,dados_professores, dados_disciplinas, dados_turmas, dados_matriculas)
+            registra_dados_estudantes(dados_estudantes)
     
 
 ##########  FUNÇÃO PARA EXCLUIR ESTUDANTES  ##########
-def excluir_estudante(dados_estudantes, dados_professores, dados_disciplinas, dados_turmas, dados_matriculas):
+def excluir_estudante(dados_estudantes):
     #Percorre a lista de estudantes em busca do código copiado
     codigo_exclusao = int(input("Qual código do estudante que deseja excluir?: "))
     for index, estudante in enumerate(dados_estudantes):
         if estudante['Código'] == codigo_exclusao:
             dados_estudantes.pop(index)
-            registra_dados(dados_estudantes, dados_professores, dados_disciplinas, dados_turmas, dados_matriculas)
+            registra_dados_estudantes(dados_estudantes)
             print("Estudante","*",estudante['Nome'],"*","foi excluido.")
 
 
@@ -186,7 +185,6 @@ def valida_cod_estudante(codigo, dados_estudantes):
 
 ##########  FUNÇÃO PARA LISTAGEM DE PROFESSORES  ##########
 def listar_professores(dados_professores):
-    dados_professores = carrega_dados()
     if len(dados_professores) == 0:
         print("A lista está vazia")
 
@@ -197,7 +195,7 @@ def listar_professores(dados_professores):
 
 
 ##########  FUNÇÃO PARA ATUALIZAR PROFESSORES ##########
-def atualizar_professores(dados_estudantes, dados_professores, dados_disciplinas, dados_turmas, dados_matriculas):
+def atualizar_professores(dados_professores):
     codigo_att = int(input("Qual o código do professor que deseja alterar?: "))
     novo_nome = input("Insira o nome: ")
     novo_cpf = input("Insira o CPF: ")
@@ -207,17 +205,17 @@ def atualizar_professores(dados_estudantes, dados_professores, dados_disciplinas
         if professor['Código'] == codigo_att:
             professor['Nome'] = novo_nome
             professor['CPF'] = novo_cpf
-            registra_dados(dados_estudantes, dados_professores, dados_disciplinas, dados_turmas, dados_matriculas)
+            registra_dados_professores(dados_professores)
 
 
 ##########  FUNÇÃO PARA EXCLUIR PROFESSORES  ##########
-def excluir_professores(dados_estudantes, dados_professores, dados_disciplinas, dados_turmas, dados_matriculas):
+def excluir_professores(dados_professores):
     #Percorre a lista em busca do código copiado
     codigo_exclusao = int(input("Qual código do estudante que deseja excluir?: "))
     for index, professor in enumerate(dados_professores):
         if professor['Código'] == codigo_exclusao:
             dados_professores.pop(index)
-            registra_dados(dados_estudantes, dados_professores, dados_disciplinas, dados_turmas, dados_matriculas)
+            registra_dados_professores(dados_professores)
             print("Professor","*",professor['Nome'],"*","foi excluido.")
 
 
@@ -231,18 +229,18 @@ def valida_cod_professores(codigo, dados_professores):
 
 ##########  FUNÇÃO PARA LISTAGEM DE DISCIPLINAS  ##########
 def listar_disciplinas(dados_disciplinas):
-    dados_disciplinas = carrega_dados()
+    dados_disciplinas = carrega_dados_disciplinas()
     if len(dados_disciplinas) == 0:
         print("A lista está vazia")
 
     else:
-        print('********** LISTA DE PROFESSORES **********')
+        print('********** LISTA DE DISCIPLINAS **********')
         for disciplina in dados_disciplinas:
             print(disciplina)
 
 
 ##########  ATUALIZA DISCIPLINAS  ##########
-def atualizar_disciplinas(dados_estudantes, dados_professores, dados_disciplinas, dados_turmas, dados_matriculas):
+def atualizar_disciplinas(dados_disciplinas):
     codigo_att = int(input("Qual o código da disciplina que deseja alterar?: "))
     novo_nome = input("Insira o nome: ")
     #PERCORRE A LISTA PARA ENCONTRAR O CODIGO INFORMADO
@@ -250,17 +248,17 @@ def atualizar_disciplinas(dados_estudantes, dados_professores, dados_disciplinas
     #SUBSTITUI AS INFORMAÇÕES RESPECTIVAMENTE
         if disciplina['Código'] == codigo_att:
             disciplina['Nome'] = novo_nome
-            registra_dados(dados_estudantes, dados_professores, dados_disciplinas, dados_turmas, dados_matriculas)
+            registra_dados_disciplinas(dados_disciplinas)
 
 
 ##########  FUNÇÃO PARA EXCLUIR DISCIPLINAS  ##########
-def excluir_disciplinas(dados_estudantes, dados_professores, dados_disciplinas, dados_turmas, dados_matriculas):
+def excluir_disciplinas(dados_disciplinas):
     #Percorre a lista em busca do código copiado
     codigo_exclusao = int(input("Qual código da disciplina que deseja excluir?: "))
     for index, disciplina in enumerate(dados_disciplinas):
         if disciplina['Código'] == codigo_exclusao:
             dados_disciplinas.pop(index)
-            registra_dados(dados_estudantes, dados_professores, dados_disciplinas, dados_turmas, dados_matriculas)
+            registra_dados_disciplinas(dados_disciplinas)
             print("Disciplina","*",disciplina['Nome'],"*","foi excluida.")
 
 
@@ -272,6 +270,16 @@ def valida_cod_disciplinas(codigo, dados_disciplinas):
             return True
         
 
+########## INSERE PROFESSORES EM TURMAS ##########
+def insere_professores_em_turmas(dados_turmas, dados_professores):
+    cod_professor = int(input("Insira o código do professor: "))
+    for professor in dados_professores:
+        if professor['Código'] == cod_professor:
+            infoturmas = {"Professor":professor}
+            dados_turmas.append(infoturmas)
+            print(dados_turmas)
+
+
 ##########  AVISO DE DESENVOLVIMENTO  ##########
 def em_desenvolvimento():
      print("Em desenvolvimento")
@@ -282,14 +290,11 @@ def menu_estudantes():
     while True:
         opcoes_menu_operacoes()
         retorno_menu = int(input())
-        dados_estudantes = carrega_dados()
-        dados_professores = carrega_dados()
-        dados_disciplinas = carrega_dados()
-        dados_turmas = carrega_dados()
-        dados_matriculas = carrega_dados()
+        dados_estudantes = carrega_dados_estudantes()
+        
         if retorno_menu == 1:
-            inserir(op, dados_estudantes, dados_professores, dados_disciplinas, dados_turmas, dados_matriculas)
-            registra_dados(dados_estudantes, dados_professores, dados_disciplinas, dados_turmas, dados_matriculas)
+            inserir(op,dados_estudantes, dados_professores, dados_disciplinas, dados_turmas, dados_matriculas)
+            registra_dados_estudantes(dados_estudantes)
             titulo_menu_estudantes()    
         
         elif retorno_menu == 2:
@@ -298,11 +303,11 @@ def menu_estudantes():
                 
         elif retorno_menu == 3: 
             titulo_menu_estudantes()
-            atualizar_estudante(dados_estudantes, dados_professores, dados_disciplinas, dados_turmas, dados_matriculas)
+            atualizar_estudante(dados_estudantes)
             
         elif retorno_menu == 4:
             titulo_menu_estudantes()
-            excluir_estudante(dados_estudantes, dados_professores, dados_disciplinas, dados_turmas, dados_matriculas)
+            excluir_estudante(dados_estudantes)
             
         elif retorno_menu == 5:  
             break
@@ -317,28 +322,24 @@ def menu_professores():
     while True:
         opcoes_menu_operacoes()
         retorno_menu = int(input())
-        dados_estudantes = carrega_dados()
-        dados_professores = carrega_dados()
-        dados_disciplinas = carrega_dados()
-        dados_turmas = carrega_dados()
-        dados_matriculas = carrega_dados()
+        dados_professores = carrega_dados_professores()
+
         if retorno_menu == 1:
-            inserir(op, dados_estudantes, dados_professores, dados_disciplinas, dados_turmas, dados_matriculas)
-            registra_dados(dados_estudantes, dados_professores, dados_disciplinas, dados_turmas, dados_matriculas)
+            inserir(op,dados_estudantes, dados_professores, dados_disciplinas, dados_turmas, dados_matriculas)
+            registra_dados_professores(dados_professores)
             titulo_menu_professores()    
         
         elif retorno_menu == 2:
-            dados_professores = carrega_dados()
             listar_professores(dados_professores)
             titulo_menu_professores()
                 
         elif retorno_menu == 3: 
             titulo_menu_professores()
-            atualizar_professores(dados_estudantes, dados_professores, dados_disciplinas, dados_turmas, dados_matriculas)
+            atualizar_professores(dados_professores)
             
         elif retorno_menu == 4:
             titulo_menu_professores()
-            excluir_professores(dados_estudantes, dados_professores, dados_disciplinas, dados_turmas, dados_matriculas)
+            excluir_professores(dados_professores)
             
         elif retorno_menu == 5:  
             break
@@ -352,28 +353,24 @@ def menu_disciplinas():
         while True:
             opcoes_menu_operacoes()
             retorno_menu = int(input())
-            dados_estudantes = carrega_dados()
-            dados_professores = carrega_dados()
-            dados_disciplinas = carrega_dados()
-            dados_turmas = carrega_dados()
-            dados_matriculas = carrega_dados()
+            dados_disciplinas = carrega_dados_disciplinas()
+
             if retorno_menu == 1:
-                inserir(op, dados_estudantes, dados_professores, dados_disciplinas, dados_turmas, dados_matriculas)
-                registra_dados(dados_estudantes,dados_professores, dados_disciplinas, dados_turmas, dados_matriculas)
+                inserir(op,dados_estudantes, dados_professores, dados_disciplinas, dados_turmas, dados_matriculas)
+                registra_dados_disciplinas(dados_disciplinas)
                 titulo_menu_disciplinas()    
             
             elif retorno_menu == 2:
-                dados_disciplinas = carrega_dados()
                 listar_disciplinas(dados_disciplinas)
                 titulo_menu_disciplinas()
                     
             elif retorno_menu == 3: 
                 titulo_menu_disciplinas()
-                atualizar_disciplinas(dados_estudantes,dados_professores, dados_disciplinas, dados_turmas, dados_matriculas)
+                atualizar_disciplinas(dados_disciplinas)
                 
             elif retorno_menu == 4:
                 titulo_menu_disciplinas()
-                excluir_disciplinas(dados_estudantes,dados_professores, dados_disciplinas, dados_turmas, dados_matriculas)
+                excluir_disciplinas(dados_disciplinas)
                 
             elif retorno_menu == 5:  
                 break
@@ -382,6 +379,37 @@ def menu_disciplinas():
                 print("Opção inválida, tente novamente!")
                 continue
 
+
+########### MENU TURMAS ###########
+def menu_turmas():
+    while True:
+            opcoes_menu_operacoes()
+            retorno_menu = int(input())
+            dados_turmas = carrega_dados_turmas()
+
+            if retorno_menu == 1:
+                inserir(op,dados_estudantes, dados_professores, dados_disciplinas, dados_turmas, dados_matriculas)
+                registra_dados_turmas(dados_turmas)
+                titulo_menu_turmas()    
+            
+            elif retorno_menu == 2:
+                listar_turmas()
+                titulo_menu_turmas()
+                    
+            elif retorno_menu == 3: 
+                titulo_menu_turmas()
+                atualizar_turmas(dados_turmas)
+                
+            elif retorno_menu == 4:
+                titulo_menu_turmas()
+                excluir_turmas(dados_turmas)
+                
+            elif retorno_menu == 5:  
+                break
+
+            else:
+                print("Opção inválida, tente novamente!")
+                continue
 
 ##########  OPÇÕES DO MENU DE OPERAÇÕES  ##########
 def opcoes_menu_operacoes():
@@ -403,7 +431,7 @@ def titulo_menu_disciplinas():
         print("***** [DISCIPLINAS] MENU DE OPERAÇÕES *****") 
 
 def titulo_menu_turmas():
-         print("***** [TURMA] MENU DE OPERAÇÕES *****")
+         print("***** [TURMAS] MENU DE OPERAÇÕES *****")
 
 def titulo_menu_matriculas(): 
         print("***** [MATRÍCULAS] MENU DE OPERAÇÕES *****")
@@ -429,7 +457,7 @@ def menu_principal(op):
 
         elif op == 4:
             titulo_menu_turmas()
-            em_desenvolvimento()
+            menu_turmas()
             break
 
         elif op == 5:
@@ -443,6 +471,12 @@ def menu_principal(op):
                 
 
 ##########  PROGRAMA PRINCIPAL  ##########
+print("***** INFORMAÇÕES ARMAZENADAS *****")
+dados_estudantes = carrega_dados_estudantes()
+dados_professores = carrega_dados_professores()
+dados_disciplinas = carrega_dados_professores()
+dados_turmas = carrega_dados_turmas()
+dados_matriculas = carrega_dados_matriculas()
 op = int      
 while True:
     print("----- MENU PRINCIPAL -----")
